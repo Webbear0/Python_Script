e5017fbe6f4bf3660a1f54b7edd5d78a14dc8fb2
webbear443 <webbear.404@gmail.com>
2023-07-20 17:37:19 +0800

    词云

--- word_cloud.py
+++ word_cloud.py
@@ -6,38 +6,45 @@ import jieba.posseg as pseg
 import wordcloud,tkinter
 from PIL import Image, ImageTk
 
-file_name = input('请输入需要转换的文件名:')
-file = open(f'{file_name}', encoding='utf-8')
-str = file.read()
-file.close()
-wlist = pseg.lcut(str)
-wtimes = {}
-cstr = []
-sw = []
+def word_cloud(file_name, font_name, img_width, img_height):
+    file = open(f'{file_name}', encoding='utf-8')
+    wtimes = {}
+    cstr = []
+    sw = []
+    str = file.read()
+    file.close()
 
-for a in wlist:
-    if a.flag == 'nr':
-        wtimes[a.word] = wtimes.get(a.word, 0) + 1
-        cstr.append(a.word)
-    else:
-        sw.append(a.word)
-wlist = list(wtimes.keys())
-wlist.sort(key=lambda x: wtimes[x], reverse=True)
-for a in wlist[:10]:
-    print(a, wtimes[a], sep='\t')
-text = ' '.join(cstr)
+    wlist = pseg.lcut(str)
+    for a in wlist:
+        if a.flag == 'nr':
+            wtimes[a.word] = wtimes.get(a.word, 0) + 1
+            cstr.append(a.word)
+        else:
+            sw.append(a.word)
+    # 提取关键字
+    wlist = list(wtimes.keys())
+    wlist.sort(key=lambda x: wtimes[x], reverse=True)
+    for a in wlist[:10]:
+        print(a, wtimes[a], sep='\t')
+    text = ' '.join(cstr)
+    # 生成图片文件
+    cloud = wordcloud.WordCloud(font_path=font_name, background_color='white'
+                                , stopwords=sw, collocations=False
+                                , width=img_width, height=img_height).generate(text)
 
-font_name = input('请输入字体文件名:')
-img_width =  int(input('请输入转换的图片高度:'))
-img_height =  int(input('请输入转换的图片宽度:'))
-cloud = wordcloud.WordCloud(font_path=font_name, background_color='white'
-                            , stopwords=sw, collocations=False
-                            , width=img_width, height=img_height).generate(text)
+    file = cloud.to_file(f'{file_name}.jpg')
+    root = tkinter.Tk()
+    img = Image.open(f'{file_name}.jpg')
+    pic = ImageTk.PhotoImage(img)
+    imgLabel = tkinter.Label(root, image=pic)
+    imgLabel.pack()
+    root.mainloop()
 
-file = cloud.to_file(f'{file_name}.jpg')
-root = tkinter.Tk()
-img = Image.open(f'{file_name}.jpg')
-pic = ImageTk.PhotoImage(img)
-imgLabel = tkinter.Label(root, image=pic)
-imgLabel.pack()
-root.mainloop()
+if __name__ == "__main__":
+    print("词云系统\n")
+    # 使用前请将txt格式文档与ttc格式字体保存到程序目录中
+    file_name = input('请输入需要转换的文档名:')
+    font_name = input('请输入字体的文件名:')
+    img_width = int(input('请输入转换的图片高度:'))
+    img_height = int(input('请输入转换的图片宽度:'))
+    word_cloud(file_name, font_name, img_width, img_height)
